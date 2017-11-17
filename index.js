92925c2a07782d13a74beba280af3f851d59318c
Onur Eker <yonureker@gmail.com>
2017-11-17 00:52:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function scuberGreetingForFeet(feet){
 }
 
 function ternaryCheckCity(city){
-  if (city = 'NYC'){
+  if (city == 'NYC'){
     return('Ok, sounds good');
   } else if (city != 'NYC'){
     return('No go');
