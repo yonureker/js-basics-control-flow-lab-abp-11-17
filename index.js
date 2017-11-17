6ddf54f5a8042c058b19f94332491b3528cc13ef
Onur Eker <yonureker@gmail.com>
2017-11-17 00:46:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function scuberGreetingForFeet(feet){
   if (feet <= 400) {
     return('This one is on me!');
-  }  else if ( feet > 2000){
+  }  else if ( feet > 2000 && feet < 2500){
     return ('I will gladly take your thirty bucks.');
   }  else if (feet > 2500){
     return ('No can do.');
