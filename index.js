2317b4bdb83fee6f17e0c9d80cfe81aa6097e8a0
Onur Eker <yonureker@gmail.com>
2017-11-17 01:19:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return('This one is on me!');
+  }  else if ( feet > 2000 && feet < 2500){
+    return ('I will gladly take your thirty bucks.');
+  }  else if (feet > 2500){
+    return ('No can do.');
+  }
+}
+
+function ternaryCheckCity(city){
+  if (city == 'NYC'){
+    return('Ok, sounds good.');
+  } else if (city !== 'NYC'){
+    return('No go.');
+  }
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+  }
+}
